b30694a1a34a365a6ed2574f7fb9a9ccc322308c
piro <piro.haniwa@gmail.com>
2026-08-22 05:50:44 +0900

    chore(opentest): add a page for deciding where a book should open

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,13 @@ const config = {
   siteUrl: "https://kindlf.kkweb.io/",
   generateRobotsTxt: true,
   // 圏外のときだけ出る画面、端末ごとの道具、ページではないもの
-  exclude: ["/~offline", "/settings", "/manifest.webmanifest", "/opengraph-image"],
+  exclude: [
+    "/~offline",
+    "/settings",
+    "/opentest",
+    "/manifest.webmanifest",
+    "/opengraph-image",
+  ],
 };
 
 module.exports = config;
